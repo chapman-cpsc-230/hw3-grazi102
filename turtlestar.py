c656b936fec3445967e12f497aa7ec4561f6c8ce
M. Andrew Moshier <moshier@chapman.edu>
2016-03-13 11:53:45 -0700

    Comment added by Dr M

--- turtlestar.py
+++ turtlestar.py
@@ -19,10 +19,11 @@ side_len = raw_input ("Enter length of each side:")
 side_len = int(side_len)
 
 
-
+### MAM: Why wrap the `i` loop inside a loop that does one iteration. That's a waste.
 for j in range(1):
     for i in range (n):
         t.forward(side_len)
+### MAM This is hard-wired for a particular number of points.
         t.left(163.5)
 stopper = raw_input ("Hit enter to quit")
 turtle.bye()
